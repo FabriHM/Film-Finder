d47905c31439564cee8408ee21407a2e2e64912a
Fabricio Huillca <102436134+FabriHM@users.noreply.github.com>
2023-11-11 01:03:16 -0500

    feat: added generate data

---
+++ generate_data.py
@@ -0,0 +1,22 @@
+from faker import Faker
+import csv
+
+fake = Faker()
+
+# Configurar el generador de números aleatorios para obtener datos consistentes
+Faker.seed(0)
+
+# Generar 100 personas y escribir los datos en un archivo CSV
+with open("users.csv", mode="w", newline="") as file:
+    writer = csv.writer(file)
+
+    for _ in range(1500):
+        dni = fake.unique.random_number(digits=8)
+        nombre_completo = fake.name()
+        fecha_nacimiento = fake.date_of_birth(minimum_age=18, maximum_age=65).strftime("%Y-%m-%d")
+        telefono = fake.phone_number()
+        email = fake.email()
+        nombre_usuario = fake.user_name()
+        contrasena = fake.password()
+
+        writer.writerow([dni, nombre_completo, fecha_nacimiento, telefono, email, nombre_usuario, contrasena])
