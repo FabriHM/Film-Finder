e013fd9a77b09c15698ab0c289d8968f4cd2bfe7
3214234442 <114323722+userxx1000@users.noreply.github.com>
2023-11-11 10:13:44 -0500

    Update login.py

--- login.py
+++ login.py
@@ -4,11 +4,11 @@ import csv
 from interfaceUI import MainWindow
 
 class LoginApp:
-    def show_registration_window(self):
+    def show_register_window(self):
         self.root.withdraw()
         from register import RegisterApp  # Import locally to avoid circular import
-        registration_app = RegisterApp(tk.Toplevel(self.root), self)
-        registration_app.show_window()
+        register_app = RegisterApp(tk.Toplevel(self.root), self)
+        register_app.show_window()
 
     def __init__(self, root):
         self.root = root
@@ -18,17 +18,17 @@ class LoginApp:
         self.label_user = tk.Label(root, text="User:")
         self.label_password = tk.Label(root, text="Password:")
 
-        # Input fields
+        # Entry fields
         self.entry_user = tk.Entry(root)
         self.entry_password = tk.Entry(root, show="*")
 
         # Buttons
         self.button_login = tk.Button(
             root, text="Login", command=self.login)
-        self.button_registration = tk.Button(
-            root, text="Register", command=self.show_registration_window)
+        self.button_register = tk.Button(
+            root, text="Register", command=self.show_register_window)
 
-        # Element positioning in the interface
+        # Positioning elements in the interface
         self.label_user.grid(row=0, column=0, padx=10, pady=5, sticky=tk.W)
         self.label_password.grid(
             row=1, column=0, padx=10, pady=5, sticky=tk.W)
@@ -37,17 +37,17 @@ class LoginApp:
         self.entry_password.grid(row=1, column=1, padx=10, pady=5)
 
         self.button_login.grid(row=2, column=1, pady=10)
-        self.button_registration.grid(row=3, column=1, pady=10)
+        self.button_register.grid(row=3, column=1, pady=10)
 
     def login(self):
-        # Get values from input fields
+        # Get values from entry fields
         user = self.entry_user.get()
         password = self.entry_password.get()
 
         # Validate that both user and password are entered
         if not user or not password:
             messagebox.showerror(
-                "Error", "Please enter user and password.")
+                "Error", "Please enter both user and password.")
             return
 
         # Login logic
@@ -56,7 +56,7 @@ class LoginApp:
             reader = csv.reader(file)
             for row in reader:
                 if row[5] == user and row[6] == password:
-                    messagebox.showinfo("Success", "Login successful.")
+                    messagebox.showinfo("Success", "Successful login.")
                     main_window = MainWindow()
                     main_window.mainloop()
                     return
@@ -69,5 +69,5 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = LoginApp(root)
 
-    # Start the main loop of the application
+    # Start the main application loop
     root.mainloop()
